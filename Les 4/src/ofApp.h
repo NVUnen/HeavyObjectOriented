d39fec8a9b76fee77837443c80821fed47fecf92
NVUnen <nielsvanunen@gmail.com>
2016-10-30 14:44:56 +0100

    LES 4 Final
    Helaas niet gelukt om een max particles mee te geven. Verder wel gelukt

--- Les 4/src/ofApp.h
+++ Les 4/src/ofApp.h
@@ -4,7 +4,7 @@
 #include "Particle.h"
 #include "ParticleEmitter.hpp"
 
-#define MAX_PARTICLES 25
+#define MAX_PARTICLES 50
 
 class ofApp : public ofBaseApp {
     
@@ -20,4 +20,7 @@ public:
     ParticleEmitter emitter1;
     ParticleEmitter emitter2;
     ParticleEmitter emitter3;
+    
+    Particle* partic[MAX_PARTICLES];
 };
+
